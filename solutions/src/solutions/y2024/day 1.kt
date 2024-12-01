18a01ee05a50f125ad0cc54e9cbfbd028e896003
Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>
2024-12-01 06:43:38 +0100

    Day 1 2024

---
+++ solutions/src/solutions/y2024/day 1.kt
@@ -0,0 +1,79 @@
+@file:Suppress("PackageDirectoryMismatch", "UnusedImport")
+
+package solutions.y2024
+
+
+/*
+
+import me.kroppeb.aoc.helpers.*
+import me.kroppeb.aoc.helpers.collections.*
+import me.kroppeb.aoc.helpers.collections.list.*
+import me.kroppeb.aoc.helpers.collections.extensions.*
+import me.kroppeb.aoc.helpers.graph.*
+import me.kroppeb.aoc.helpers.grid.*
+import me.kroppeb.aoc.helpers.point.*
+import me.kroppeb.aoc.helpers.sint.*
+import itertools.*
+import java.util.Comparator
+import java.util.ArrayDeque
+import java.util.PriorityQueue
+import kotlin.*
+import kotlin.annotation.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+import kotlin.math.*
+
+ */
+
+
+import me.kroppeb.aoc.helpers.*
+import me.kroppeb.aoc.helpers.collections.*
+import me.kroppeb.aoc.helpers.collections.list.*
+import me.kroppeb.aoc.helpers.collections.extensions.*
+import me.kroppeb.aoc.helpers.graph.*
+import me.kroppeb.aoc.helpers.grid.*
+import me.kroppeb.aoc.helpers.point.*
+import me.kroppeb.aoc.helpers.sint.*
+import itertools.*
+import log
+import java.util.Comparator
+import java.util.ArrayDeque
+import java.util.PriorityQueue
+import kotlin.*
+import kotlin.annotation.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+import kotlin.math.*
+
+
+private val xxxxx = Clock(6, 3)
+//private val xxxxy = YCombSettings.useMemoization(true)
+//private val xxxxz = LoggerSettings.logNonAnswers(false)
+
+
+
+private fun part1() {
+	var inp = getLines(1).ints().transpose().map{it.sorted()}.transpose().sumOf { (a,b) -> abs(b - a) } log 1
+}
+
+private fun part2() {
+	var (left, right) = getLines(1).ints().transpose()
+
+	val c = right.countEach()
+
+	left.sumOf {c.getOrDefault(it, 0.s) * it} log 2
+}
+
+fun main() {
+	println("Day 1: ")
+	part1()
+	part2()
+}
