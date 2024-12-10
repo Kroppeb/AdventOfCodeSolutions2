5d44673b48068c7090052e96a4e0df08f1c7a739
Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>
2024-12-10 06:28:17 +0100

    day 10

---
+++ solutions/src/solutions/y2024/day 10.kt
@@ -0,0 +1,126 @@
+@file:Suppress("PackageDirectoryMismatch", "UnusedImport")
+
+package solutions.y2024
+
+
+/*
+
+import me.kroppeb.aoc.helpers.*
+import me.kroppeb.aoc.helpers.collections.*
+import me.kroppeb.aoc.helpers.collections.list.*
+import me.kroppeb.aoc.helpers.collections.extensions.*
+import me.kroppeb.aoc.helpers.graph.*
+import me.kroppeb.aoc.helpers.grid.*
+import me.kroppeb.aoc.helpers.point.*
+import me.kroppeb.aoc.helpers.sint.*
+import itertools.*
+import java.util.Comparator
+import java.util.ArrayDeque
+import java.util.PriorityQueue
+import kotlin.*
+import kotlin.annotation.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+import kotlin.math.*
+
+ */
+
+
+import log
+import me.kroppeb.aoc.helpers.*
+import me.kroppeb.aoc.helpers.collections.list.toH
+import me.kroppeb.aoc.helpers.grid.*
+import me.kroppeb.aoc.helpers.point.*
+import me.kroppeb.aoc.helpers.sint.*
+import kotlin.*
+import kotlin.collections.*
+import kotlin.io.*
+
+
+private val xxxxx = Clock(6, 3)
+//private val xxxxy = YCombSettings.useMemoization(true)
+//private val xxxxz = LoggerSettings.logNonAnswers(false)
+
+
+private fun part1() {
+	var inp = getLines(2024, 10)
+//	var inp = pre(10, 0)
+	var hob = inp.digits().grid()
+
+	hob.filter{it.v==0}.sumOf { start ->
+		val seen = mutableSetOf<BoundedGridPoint<Int>>()
+
+		val a = ArrayDeque(listOf(start))
+		var c = 0
+
+		while (a.isNotEmpty()) {
+			val pp = a.removeFirst()
+			if (pp in seen) {
+				continue
+			}
+
+			if (pp.v == 9){
+				c++
+			}
+
+			seen.add(pp)
+
+			for (i in pp.getQuadNeighbours()){
+				if (i.v - pp.v == 1){
+					a.add(i)
+				}
+			}
+		}
+
+
+		c
+	} log 1
+}
+
+private fun part2() {
+	var inp = getLines(2024, 10)
+//	var inp = pre(10, 0)
+	var hob = inp.digits().grid()
+
+	hob.filter{it.v==0}.sumOf { start ->
+		val seen = mutableSetOf<BoundedGridPoint<Int>>()
+		val back = mutableMapOf(start to mutableListOf<BoundedGridPoint<Int>>())
+
+		val a = ArrayDeque(listOf(start))
+
+		while (a.isNotEmpty()) {
+			val pp = a.removeFirst()
+			if (pp in seen) {
+				continue
+			}
+			seen.add(pp)
+
+			for (i in pp.getQuadNeighbours()){
+				if (i.v - pp.v == 1){
+					a.add(i)
+					back.getOrPut(i) { mutableListOf()}.add(pp)
+				}
+			}
+		}
+
+		val backScore = mutableMapOf(start to 1.s)
+		for (i in 1..9) {
+			seen.filter { it.v == i }.forEach {
+				backScore[it] = back[it]!!.sumOf { backScore[it]!! }
+			}
+		}
+
+		seen.filter { it.v == 9 } .sumOf { backScore[it]!! }
+	} log 2
+}
+
+
+fun main() {
+	println("Day 10: ")
+	part1()
+	part2()
+}
